509df8e829b294834891e4c297d78632e15720a1
Seb Amos <sebastian.amos8@gmail.com>
2022-08-15 16:19:45 +0100

    test5

--- src/server.js
+++ src/server.js
@@ -4,20 +4,20 @@ const express = require('express');
 const userRouter = require('./user/routes');
 const port = process.env.PORT || 5000;
 
-// var bodyParser = require('body-parser');
+var bodyParser = require('body-parser');
 const app = express();
 
 app.use(express.json());
 app.use(cors());
-// app.use(bodyParser.json({
-//     limit: '50mb'
-//   }));
+app.use(bodyParser.json({
+    limit: '50mb'
+  }));
   
-//   app.use(bodyParser.urlencoded({
-//     limit: '50mb',
-//     parameterLimit: 100000,
-//     extended: true 
-//   }));
+  app.use(bodyParser.urlencoded({
+    limit: '50mb',
+    parameterLimit: 100000,
+    extended: true 
+  }));
 
 app.use(userRouter);
 
